5c1e7753af473ec15931249a947631bace1d2114
Nate Luzod <nate.luzod@mac.com>
2018-01-28 11:03:32 -1000

    Adding gulp and minification

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
+
+gulp.task('minify', function() {
+  return gulp.src(['./public/*.html', './public/**/*.html'])
+    .pipe(htmlmin({
+      removeComments: true,
+      minifyCSS: true,
+      minifyJS: true,
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest('./public'));
+});
